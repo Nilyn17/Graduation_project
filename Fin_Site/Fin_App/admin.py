7227cae1e1d5fc42d4447e37da23696480db845f
Nilyn_17 <nilyn2@yandex.ru>
2022-12-16 21:28:02 +0300

    models complete 1.2

--- Fin_Site/Fin_App/admin.py
+++ Fin_Site/Fin_App/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Space)
+class SpaceAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SpandingCategories)
+class SpandingCategoriesAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Spanding)
+class SpandingAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SpaceLogs)
+class SpaceLogsAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(ReferalCode)
+class ReferalCodeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Shit)
+class ShitAdmin(admin.ModelAdmin):
+    pass
--- Fin_Site/Fin_App/models.py
+++ Fin_Site/Fin_App/models.py
@@ -1,19 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db.models import Sum
 
-# Create your models here.
 
 currency_choices = (("USD", "USD"), ("EUR", "EUR"), ("BYN", "BYN"))
 
 
-class BalanceLessZeroError(Exception):
-    """BalanceLessZeroError."""
+class CustomUser(models.Model):
+    username = models.CharField(max_length=50)
 
-class CustomUser(AbstractUser):
     telegram_id = models.PositiveIntegerField(null=True)
     
-
 class Space(models.Model):
     title = models.CharField(max_length=25)
     currency = models.CharField(max_length=3, choices=currency_choices)
@@ -25,51 +20,39 @@ class Space(models.Model):
         through="Shit"
     )
 
-    dick_list = models.ManyToManyField(
-        "Ban_and_KickList",
-        related_name="banlist",
-        through="Dick"
-    )
-
     @property
-    def spend(self):
-        # money = sum()
+    def cost_amount(self):
         return 
-
-    def __str__(self) -> str:
-        return f"{self.user.username} | balance: {self.balance}"
 
 class Shit(models.Model):
     perks_choises = (
         ("A", "admin user"), 
         ("C", "casual user"), 
-        ("M", "master user"),
-        ("B", "banned user"), 
+        ("M", "master user"), 
         )
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     space = models.ForeignKey(Space, on_delete=models.CASCADE)
     user_perk = models.CharField(max_length=1, choices=perks_choises)
-
-
+    is_banned = models.BooleanField(default=False)
 
 class SpandingCategories(models.Model):
     space = models.ForeignKey(Space, on_delete=models.CASCADE)
-    title = models.CharField(max_length=9999)
+    title = models.CharField(max_length=200)
 
     def __str__(self) -> str:
         return self.title
 
 class Spanding(models.Model):
-
+    space = models.ForeignKey(Space, on_delete=models.CASCADE)
     category = models.ForeignKey(SpandingCategories, on_delete=models.CASCADE)
     currency = models.CharField(max_length=3, choices=currency_choices)
-    spend = models.PositiveIntegerField()
+    expense = models.PositiveIntegerField()
     spanding_time = models.DateTimeField(auto_now_add=True)
 
 class SpaceLogs(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     action_time = models.DateTimeField(auto_now_add=True)
-    action = models.CharField()
+    action = models.TextField()
 
 class ReferalCode(models.Model):
     code = models.CharField(max_length=255)                             #change !!!
